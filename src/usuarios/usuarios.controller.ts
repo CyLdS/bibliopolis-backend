55b2263498a22fb34f35536583c381fea79f6cd3
CydS <cindyluanadasilva@gmail.com>
2024-07-27 16:39:26 -0300

    corrigiendo resticciones de busqueda para usuarios

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,38 +1,49 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Res } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { Usuario } from 'src/models/Usuario';
+import { response, Response } from 'express';
+import { error } from 'console';
 
 @Controller('usuarios')
 export class UsuariosController {
 
 
-    constructor(private readonly servicio:UsuariosService){}
+    constructor(private readonly servicio: UsuariosService) { }
 
     // Registrar un nuevo usuario
     @Post()
-    crearUsuario( @Body() user:Usuario ):void{
-      this.servicio.crearUsuario;
-      
+    crearUsuario(@Body() user: Usuario, @Res() response: Response) {
+        const nuevoUsuario = this.servicio.crearUsuario(user);
+        if (nuevoUsuario) {
+            response.status(201).send(nuevoUsuario);
+        } else {
+            response.status(400).send({ error: 'usuario no creado' })
+        }
     }
-    
+
     // Obtener un usuario según su id
-     @Get(':id')
-     obtenerUsuarioPorId( @Param('id') id:number ): Usuario{
-         return this.servicio.obtenerUsuarioPorId(id);
-     }
-    
-       // Obtener todos los usuarios
-       @Get()
-       obtenerUsuarios():Usuario[] {
-           return this.servicio.obtenerUsuarios();
-       }
-    
-      // Eliminar un usuario según su id
-      @Delete(':id')
-      eliminarUsuario(@Param('id') id: number):void{
-          this.servicio.eliminarUsuario(id);
-      }
-    
+    @Get(':id')
+    obtenerUsuarioPorId(@Param('id') id: number, @Res() response: Response) {
+        const usuarioUnico = this.servicio.obtenerUsuarioPorId(id);
+        if (usuarioUnico) {
+            response.status(200).send(usuarioUnico);
+        } else {
+            response.status(404).send({ error: 'usuario no existe' });
+        }
+    }
+
+    // Obtener todos los usuarios
+    @Get()
+    obtenerUsuarios(): Usuario[] {
+        return this.servicio.obtenerUsuarios();
+    }
+
+    // Eliminar un usuario según su id
+    @Delete(':id')
+    eliminarUsuario(@Param('id') id: number): void {
+        this.servicio.eliminarUsuario(id);
+    }
+
 
 
 
--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -7,10 +7,18 @@ export class UsuariosService {
     private usuarios: Usuario[] = [];
     
     //     Registrar un nuevo usuario
-    crearUsuario(usuario: Usuario): void {
+    crearUsuario(usuario: Usuario): Usuario {
+        for (let i=0; i< this.usuarios.length; i++) {
+            if (this.usuarios[i].correoElectronico == usuario.correoElectronico) {
+              return null;
+            }
+          }
+          
         usuario.id = this.usuarios.length + 1;
         this.usuarios.push(usuario);
+        return usuario;
     }
+    
 
     // Obtener un usuario según su id
     obtenerUsuarioPorId(id: number): Usuario {
